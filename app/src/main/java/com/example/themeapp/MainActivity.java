f3d2dd3c484550840e322b94018ee3ca9e57c816
jsb999 <90780236+jsb999@users.noreply.github.com>
2022-09-09 12:09:56 -0500

    more colors

--- app/src/main/java/com/example/themeapp/MainActivity.java
+++ app/src/main/java/com/example/themeapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.themeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void nextPage(View v) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
     }
 }
