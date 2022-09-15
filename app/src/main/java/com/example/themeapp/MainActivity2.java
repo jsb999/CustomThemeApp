00dab9c8e5bf8cad26f1d8eca101402b4af4351e
jsb999 <90780236+jsb999@users.noreply.github.com>
2022-09-15 12:55:18 -0500

    added linear layout and custom button
    add blue theme later

--- app/src/main/java/com/example/themeapp/MainActivity2.java
+++ app/src/main/java/com/example/themeapp/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.themeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void nextPage(View v) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+        startActivity(intent);
     }
 }
