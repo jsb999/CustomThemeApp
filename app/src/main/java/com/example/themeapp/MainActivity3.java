578ed37a489d2ed79fd4f89862209eb062cef4f2
jsb999 <90780236+jsb999@users.noreply.github.com>
2022-09-20 12:46:58 -0500

    fixed buttons

--- app/src/main/java/com/example/themeapp/MainActivity3.java
+++ app/src/main/java/com/example/themeapp/MainActivity3.java
@@ -17,46 +17,5 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
-        buttonIDs = new int[]{R.id.toggleButton,R.id.toggleButton2,R.id.toggleButton3};
-
-    }
-
-    public void onClick(View v){
-        boolean checked = ((ToggleButton) v).isChecked();
-        switch(v.getId()) {
-            case R.id.toggleButton:
-                if(checked){
-                    Log.i("logOutput", "toggle1 is checked on");
-                } else{
-                    Log.i("logOutput", "toggle1 is checked off");
-                }
-                setOff(v.getId());
-                break;
-            case R.id.toggleButton2:
-                if(checked){
-                    Log.i("logOutput", "toggle2 is checked on");
-                } else{
-                    Log.i("logOutput", "toggle2 is checked off");
-                }
-                setOff(v.getId());
-                break;
-            case R.id.toggleButton3:
-                if(checked){
-                    Log.i("logOutput", "toggle3 is checked on");
-                } else{
-                    Log.i("logOutput", "toggle3 is checked off");
-                }
-                setOff(v.getId());
-                break;
-        }
-    }
-
-    public void setOff(int id){
-        for (int i = 0; i < buttonIDs.length; i++) {
-            if(buttonIDs[i] != id) {
-                ((ToggleButton) findViewById(buttonIDs[i])).setChecked(false);
-            }
-        }
     }
 }
